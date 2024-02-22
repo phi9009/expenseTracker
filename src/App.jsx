4124a4b490157ed755fc52eeb736d3849e9bccd0
phi9009 <phi9009@gmail.com>
2024-02-21 18:08:52 -0600

    Added Navbar and debugged issues with Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import { CssBaseline } from '@mui/material'
 import './App.css'
+import Navbar from './Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <CssBaseline />
+      <Navbar />
     </>
   )
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,25 @@
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import './Navbar.css'
+
+
+export default function Navbar() {
+  return (
+    <Box className="Navbar" sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+         
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Expense Tracker
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
